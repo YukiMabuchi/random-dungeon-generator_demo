942b4446bff015b26c574b29011aecc7d04e9459
Yuki Mabuchi <67894806+YukiMabuchi@users.noreply.github.com>
2024-08-30 23:52:06 +0900

    feat: ドアに入るとステージを再生成する

---
+++ Assets/Scripts/ExitDoorway.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Rigidbody2D), typeof(BoxCollider2D))]
+public class ExitDoorway : MonoBehaviour
+{
+    private void Reset()
+    {
+        GetComponent<Rigidbody2D>().isKinematic = true;
+        BoxCollider2D box = GetComponent<BoxCollider2D>();
+        box.size = Vector2.one * .1f;
+        box.isTrigger = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
