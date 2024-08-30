be9778dfe5ee6f53fa231c1ba937ce4d30f477e0
Yuki Mabuchi <67894806+YukiMabuchi@users.noreply.github.com>
2024-08-30 23:26:11 +0900

    feat: ランダムにタイルを生成する

--- Assets/Scripts/DungeonManager.cs
+++ Assets/Scripts/DungeonManager.cs
@@ -1,7 +1,80 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DungeonManager : MonoBehaviour
 {
-    public GameObject floorPrefab, wallPrefab;
+    public GameObject floorPrefab, wallPrefab, tilePrefab;
+    public int totalFloorCount;
+
     [HideInInspector] public float minX, maxX, minY, maxY;
+
+    List<Vector3> floorList = new List<Vector3>();
+
+    void Start()
+    {
+        RandomWalker();
+    }
+    void Update()
+    {
+        // テスト用
+        if (Application.isEditor && Input.GetKeyDown(KeyCode.Backspace))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    /// <summary>
+    /// Map Generator
+    /// </summary>
+    void RandomWalker()
+    {
+        Vector3 curPos = Vector3.zero; // x: 0, y: 0, z: 0
+
+        floorList.Add(curPos);
+
+        // floorListを作成
+        bool inFLoorList;
+        while (floorList.Count < totalFloorCount)
+        {
+            switch (Random.Range(1, 5))
+            {
+                case 1:
+                    curPos += Vector3.up;
+                    break;
+                case 2:
+                    curPos += Vector3.right;
+                    break;
+                case 3:
+                    curPos += Vector3.down;
+                    break;
+                case 4:
+                    curPos += Vector3.left;
+                    break;
+            }
+
+            inFLoorList = false;
+            for (int i = 0; i < floorList.Count; i++)
+            {
+                if (Vector3.Equals(curPos, floorList[i]))
+                {
+                    inFLoorList = true;
+                    break;
+                }
+            }
+            if (!inFLoorList)
+            {
+                floorList.Add(curPos);
+            }
+        }
+
+        // floorListを元にタイルを生成
+        for (int i = 0; i < floorList.Count; i++)
+        {
+            GameObject goTile = Instantiate(tilePrefab, floorList[i], Quaternion.identity);
+            goTile.name = tilePrefab.name;
+            goTile.transform.SetParent(transform);
+        }
+
+    }
 }
