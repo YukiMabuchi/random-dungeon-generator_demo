e1cea322f9c273642bc77e1213502329787b969c
Yuki Mabuchi <67894806+YukiMabuchi@users.noreply.github.com>
2024-09-01 18:02:37 +0900

    feat: 敵の動きに放浪、追尾、攻撃を追加

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,26 +4,30 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    Vector2 curPos;
-    LayerMask obstacleMask;
-    List<Vector2> availableMovementList = new List<Vector2>();
+    public float alertRange;
+    public Vector2 patrolInterval;
+    public float chaseSpeed;
+    public Vector2 damageRange;
 
+    Player player;
+    Vector2 curPos;
+    LayerMask obstacleMask, unwalkableMask;
+    List<Vector2> availableMovementList = new List<Vector2>();
+    List<Node> nodesList = new List<Node>();
     bool isMoving;
 
     void Start()
     {
+        player = FindObjectOfType<Player>();
         obstacleMask = LayerMask.GetMask("Wall", "Enemy", "Player");
+        unwalkableMask = LayerMask.GetMask("Wall", "Enemy");
         curPos = transform.position;
+        StartCoroutine(Movement());
     }
 
-    void Update()
-    {
-        if (!isMoving)
-        {
-            Patrol();
-        }
-    }
-
+    /// <summary>
+    /// 自由に歩く関数
+    /// </summary>
     void Patrol()
     {
         availableMovementList.Clear();
@@ -62,10 +66,80 @@ public class Enemy : MonoBehaviour
             curPos += availableMovementList[randomIndex];
         }
 
-        StartCoroutine(SmoothMove());
+        StartCoroutine(SmoothMove(Random.Range(patrolInterval.x, patrolInterval.y)));
     }
 
-    IEnumerator SmoothMove()
+    // TODO
+    void Attack()
+    {
+        int roll = Random.Range(0, 100);
+        if (roll > 50)
+        {
+            float damageAmount = Mathf.Ceil(Random.Range(damageRange.x, damageRange.y));
+            Debug.Log(name + " attacked and hit for " + damageAmount + " points of damage");
+        }
+        else
+        {
+            Debug.Log(name + " attacked and missed");
+        }
+    }
+
+    /// <summary>
+    /// 確認対象のマス目がunwalableMaskではないか確認し、そうではない場合nodeListに追加する
+    /// </summary>
+    /// <param name="checkPoint"></param>
+    /// <param name="parent"></param>
+    void CheckNode(Vector2 checkPoint, Vector2 parent)
+    {
+        Vector2 hitSize = Vector2.one * .5f;
+        Collider2D hit = Physics2D.OverlapBox(checkPoint, hitSize, 0, unwalkableMask);
+        if (!hit)
+        {
+            nodesList.Add(new Node(checkPoint, parent));
+        }
+    }
+
+    Vector2 FindNextStep(Vector2 startPos, Vector2 targetPos)
+    {
+        int listIndex = 0;
+        Vector2 myPos = startPos;
+        nodesList.Clear();
+        nodesList.Add(new Node(startPos, startPos));
+        while (myPos != targetPos && listIndex < 1000 && nodesList.Count > 0) // TODO: listIndex < 1000は維持的、ダンジョンの大きさで最大値異なる
+        {
+            // up, right, down, leftの移動可能なマスを追加
+            CheckNode(myPos + Vector2.up, myPos);
+            CheckNode(myPos + Vector2.right, myPos);
+            CheckNode(myPos + Vector2.down, myPos);
+            CheckNode(myPos + Vector2.left, myPos);
+
+            listIndex++;
+            if (listIndex < nodesList.Count)
+            {
+                myPos = nodesList[listIndex].position;
+            }
+        }
+
+        // targetPosまでたどり着いた時、リストを逆にして次のマス目を選択する
+        if (myPos == targetPos)
+        {
+            nodesList.Reverse();
+            for (int i = 0; i < nodesList.Count; i++)
+            {
+                if (myPos == nodesList[i].position)
+                {
+                    if (nodesList[i].parent == startPos)
+                    {
+                        return myPos; // 次のマス目
+                    }
+                    myPos = nodesList[i].parent;
+                }
+            }
+        }
+        return startPos;
+    }
+
+    IEnumerator SmoothMove(float speed)
     {
         isMoving = true;
         while (Vector2.Distance(transform.position, curPos) > .01f)
@@ -75,8 +149,54 @@ public class Enemy : MonoBehaviour
         }
         transform.position = curPos;
 
-        yield return new WaitForSeconds(Random.Range(1f, 5f)); // TODO: プレイヤーが動くまで？
+        yield return new WaitForSeconds(speed); // TODO: プレイヤーが動くまで？
 
         isMoving = false;
+    }
+
+    /// <summary>
+    /// プレイヤーとの距離によって放浪するかプレイヤーを追いかけるかプレイヤーを攻撃する関数
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator Movement()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(.1f);
+
+            if (!isMoving)
+            {
+                // プレイヤーとの距離を取得
+                float dist = Vector2.Distance(transform.position, player.transform.position);
+                if (dist <= alertRange)
+                {
+                    // 攻撃
+                    if (dist <= 1.1f) // 1マスは1.1の方が安全
+                    {
+                        Attack();
+                        yield return new WaitForSeconds(Random.Range(.5f, 1.15f));
+                    }
+                    // 追いかける
+                    else
+                    {
+                        Vector2 newPos = FindNextStep(transform.position, player.transform.position);
+                        if (newPos != curPos)
+                        {
+                            curPos = newPos;
+                            StartCoroutine(SmoothMove(chaseSpeed));
+                        }
+                        else
+                        {
+                            Patrol();
+                        }
+                    }
+                }
+                // 放浪
+                else
+                {
+                    Patrol();
+                }
+            }
+        }
     }
 }
